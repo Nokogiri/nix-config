58d081d484489eb19b384be56c49a61bba0ff51c
nokogiri <nokogiri@gefjon.org>
2022-12-19 15:02:21 +0100

    progress.....

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,11 +3,10 @@
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
   # example = pkgs.callPackage ./example { };
-  plymouth-spinner-monochrome =
-    pkgs.callPackage ./plymouth-spinner-monochrome { };
-  ryzen-ppd = pkgs.callPackage ./ryzen-ppd { };
-  catppuccin-gtk-frappe-teal =
-    pkgs.callPackage ./catppuccin-gtk-frappe-teal { };
+  catppuccin-gtk-frappe-teal = pkgs.callPackage ./catppuccin-gtk-frappe-teal { };
   colloid-cursors = pkgs.callPackage ./colloid-cursors { };
+  heroic = pkgs.callPackage ./heroic { };
   pass-wofi = pkgs.callPackage ./pass-wofi { };
+  plymouth-spinner-monochrome = pkgs.callPackage ./plymouth-spinner-monochrome { };
+  ryzen-ppd = pkgs.callPackage ./ryzen-ppd { };
 }
