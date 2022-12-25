d0d3e380784089f105bc26954249b0e2edfd9308
nokogiri <nokogiri@gefjon.org>
2022-12-25 23:00:30 +0100

    new wallpapers from apod.nasa.gov

--- home-manager/features/desktop/wallpaper/default.nix
+++ home-manager/features/desktop/wallpaper/default.nix
@@ -1,9 +1,9 @@
 {
-  xdg.configFile."wallpaper/wall-01.png".source = ./wall-01.png;
-  xdg.configFile."wallpaper/wall-02.png".source = ./wall-02.png;
-  xdg.configFile."wallpaper/wall-03.png".source = ./wall-03.png;
-  xdg.configFile."wallpaper/wall-04.png".source = ./wall-04.png;
-  xdg.configFile."wallpaper/wall-05.png".source = ./wall-05.png;
-  xdg.configFile."wallpaper/wall-06.png".source = ./wall-06.png;
-
+  xdg.configFile."wallpaper/01.webp".source = ./Enceladus-dark.webp;
+  xdg.configFile."wallpaper/02.webp".source = ./Enceladus-stripes.webp;
+  xdg.configFile."wallpaper/03.webp".source = ./Europa.webp;
+  xdg.configFile."wallpaper/04.webp".source = ./Jupiter.webp;
+  xdg.configFile."wallpaper/05.webp".source = ./Keppler-186f.webp;
+  xdg.configFile."wallpaper/06.webp".source = ./Pluto.webp;
+  xdg.configFile."wallpaper/07.webp".source = ./Saturn.webp;
 }
