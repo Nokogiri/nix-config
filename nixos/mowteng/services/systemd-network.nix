a0aa72e862b11027e7899ae656e06cf173b5d764
nokogiri <nokogiri@gefjon.org>
2022-12-28 00:03:30 +0100

    misc

--- nixos/mowteng/services/systemd-network.nix
+++ nixos/mowteng/services/systemd-network.nix
@@ -2,6 +2,10 @@
   systemd.network.networks."21-wireless" = {
     matchConfig.Name = [ "wl*" ];
     DHCP = "yes";
+    dhcpV4Config = {
+      UseDNS = true;
+      UseRoutes = true;
+    };
     networkConfig = {
       MulticastDNS = false;
       LLMNR = false;
