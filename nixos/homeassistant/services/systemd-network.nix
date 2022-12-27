ecb1f972a2f50629ea775c68f43b055a90747919
nokogiri <nokogiri@gefjon.org>
2022-12-27 23:09:20 +0100

    static network

--- nixos/homeassistant/services/systemd-network.nix
+++ nixos/homeassistant/services/systemd-network.nix
@@ -1,11 +1,14 @@
 {
   systemd.network.networks."20-wired" = {
       matchConfig.Name = [ "en*" ];
-      DHCP = "yes";
-      dhcpV4Config = {
-        RouteMetric = 10;
-        UseDNS = true;
-        UseRoutes = true;
-      };
+      DHCP = "no";
+      address = [ "192.168.178.57" ];
+      gateway = [ "192.168.178.1" ];
+      dns = [ "192.168.178.254" ];
+      #dhcpV4Config = {
+      #  RouteMetric = 10;
+      #  UseDNS = true;
+      #  UseRoutes = true;
+      #};
     };
 }
