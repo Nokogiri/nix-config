4dd7bddd30524d094bdf110ac3d5cbaeae2013df
nokogiri <nokogiri@gefjon.org>
2022-12-28 00:07:15 +0100

    homeassistant, done?

--- nixos/homeassistant/services/systemd-network.nix
+++ nixos/homeassistant/services/systemd-network.nix
@@ -1,7 +1,7 @@
 {
   systemd.network.networks."20-wired" = {
     matchConfig.Name = [ "en*" ];
-    DHCP = "ipv6";
+    DHCP = "no";
     address = [ "192.168.178.57" ];
     gateway = [ "192.168.178.1" ];
     dns = [ "192.168.178.254" ];
