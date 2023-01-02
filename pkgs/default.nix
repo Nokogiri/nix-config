e33fef6b1eb9462ece767f00b8f0457f198489a8
nokogiri <nokogiri@gefjon.org>
2023-01-02 10:58:18 +0100

    add iio-sensor-proxy 3.4

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,6 +8,7 @@
   colloid-cursors = pkgs.callPackage ./colloid-cursors { };
   heroic-unwrapped = pkgs.callPackage ./heroic { };
   heroic = pkgs.callPackage ./heroic/fhsenv.nix { };
+  iio-sensor-proxy = pkgs.callPackage ./iio-sensor-proxy { };
   mailctl = pkgs.callPackage ./mailctl { };
   nix-stray-roots = pkgs.callPackage ./nix-stray-roots { };
   pass-fuzzel = pkgs.callPackage ./pass-fuzzel { };
