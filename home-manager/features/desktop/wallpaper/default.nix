d613302f67249192094a3796b2c3bc1e4781d992
nokogiri <nokogiri@gefjon.org>
2023-01-08 14:45:41 +0100

    png Wallpapers

--- home-manager/features/desktop/wallpaper/default.nix
+++ home-manager/features/desktop/wallpaper/default.nix
@@ -1,10 +1,3 @@
 {
   xdg.dataFile."wallpaper".source = ./pictures;
-  xdg.configFile."wallpaper/01.webp".source = ./Enceladus-dark.webp;
-  xdg.configFile."wallpaper/02.webp".source = ./Enceladus-stripes.webp;
-  xdg.configFile."wallpaper/03.webp".source = ./Europa.webp;
-  xdg.configFile."wallpaper/04.webp".source = ./Jupiter.webp;
-  xdg.configFile."wallpaper/05.webp".source = ./Keppler-186f.webp;
-  xdg.configFile."wallpaper/06.webp".source = ./Pluto.webp;
-  xdg.configFile."wallpaper/07.webp".source = ./Saturn.webp;
 }
