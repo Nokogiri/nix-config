2c7c509b3b9bb9ad5537735f39991968caeb008c
nokogiri <nokogiri@gefjon.org>
2023-01-17 14:26:36 +0100

    nix flake update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-
 { pkgs ? (import ../nixpkgs.nix) { } }: {
   # example = pkgs.callPackage ./example { };
   amdctl = pkgs.callPackage ./amdctl { };
@@ -8,6 +7,10 @@
     pkgs.callPackage ./catppuccin-gtk-frappe-teal { };
   colloid-cursors = pkgs.callPackage ./colloid-cursors { };
   iio-sensor-proxy = pkgs.callPackage ./iio-sensor-proxy { };
+  kitty = pkgs.callPackage ./kitty { };
+  #harfbuzz = harfbuzz.override { withCoreText = stdenv.isDarwin; };
+  #inherit (darwin.apple_sdk_11_0.frameworks) Cocoa CoreGraphics Foundation IOKit Kernel UniformTypeIdentifiers OpenGL UserNotifications;
+  #};
   mailctl = pkgs.callPackage ./mailctl { };
   nix-stray-roots = pkgs.callPackage ./nix-stray-roots { };
   pass-fuzzel = pkgs.callPackage ./pass-fuzzel { };
