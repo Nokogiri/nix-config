ec8858a1b7ec56271932a78634687255569b563d
nokogiri <nokogiri@gefjon.org>
2023-01-20 18:12:29 +0100

    mbpfan

--- nixos/common/optional/mbpfan.nix
+++ nixos/common/optional/mbpfan.nix
@@ -6,7 +6,7 @@ services.mbpfan = {
         high_temp = 65;
         max_temp = 89;
         polling_interval = 2;
-        min_fan1_speed = 2000;
+        min_fan1_speed = 3000;
       };
     };
   
