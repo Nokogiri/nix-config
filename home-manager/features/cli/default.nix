661bea28738b8ada8eb21cf0a841e4741205a572
nokogiri <nokogiri@gefjon.org>
2023-02-01 22:59:19 +0100

    eww start

--- home-manager/features/cli/default.nix
+++ home-manager/features/cli/default.nix
@@ -16,6 +16,10 @@
   home.sessionVariables = {
     PATH = "$PATH:$HOME/.local/bin";
     EDITOR = "${pkgs.helix}/bin/hx";
+    XDG_CACHE_HOME  = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME    = "\${HOME}/.local/bin";
+    XDG_DATA_HOME   = "\${HOME}/.local/share";
   };
 
   home.packages = with pkgs; [
--- home-manager/features/desktop/eww/default.nix
+++ home-manager/features/desktop/eww/default.nix
@@ -1,4 +1,10 @@
 { pkgs, ...}:{
+  home.packages = [
+    pkgs.gojq
+    pkgs.jaq
+    pkgs.socat
+    pkgs.wget
+  ];
   programs.eww = {
     enable = true;
     package = pkgs.eww-wayland;
