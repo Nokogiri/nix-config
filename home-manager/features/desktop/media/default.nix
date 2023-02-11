86cee3208e6b8596b3330469c441a0f39023246f
nokogiri <nokogiri@gefjon.org>
2023-02-11 09:49:45 +0100

    Stupid QT BUG

--- home-manager/features/desktop/media/default.nix
+++ home-manager/features/desktop/media/default.nix
@@ -17,4 +17,12 @@
     name = "umpv";
     settings.NoDisplay = "true";
   };
+  xdg.desktopEntries."com.github.iwalton3.jellyfin-media-player" = {
+    exec = "env env QT_WAYLAND_DISABLE_WINDOWDECORATION=0 ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer";
+    name = "Jellyfin Media Player";
+    icon = "com.github.iwalton3.jellyfin-media-player";
+    terminal = false;
+    categories = ["AudioVideo" "Video" "Player" "TV"];
+    comment = "Desktop client for Jellyfin";
+  };
 }
