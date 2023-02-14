6c4b86f87b6d61c11538531486e3181eeb32d562
nokogiri <nokogiri@gefjon.org>
2023-02-14 16:06:50 +0100

    little fix

--- home-manager/consolero.nix
+++ home-manager/consolero.nix
@@ -11,7 +11,7 @@
 
     # Or modules exported from other flakes (such as nix-colors):
     inputs.nix-colors.homeManagerModules.default
-
+    inputs.nur.nixosModules.nur
     inputs.spicetify-nix.homeManagerModule
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
