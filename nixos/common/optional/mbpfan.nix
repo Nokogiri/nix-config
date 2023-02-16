c45fd9d40135f7bde929fcbaeaadb7ff43874104
nokogiri <nokogiri@gefjon.org>
2023-02-16 10:13:48 +0100

    tiny

--- nixos/common/optional/mbpfan.nix
+++ nixos/common/optional/mbpfan.nix
@@ -2,9 +2,9 @@
   services.mbpfan = {
     enable = true;
     settings.general = {
-      low_temp = 50;
-      high_temp = 65;
-      max_temp = 89;
+      low_temp = 55;
+      high_temp = 70;
+      max_temp = 85;
       polling_interval = 2;
       min_fan1_speed = 3000;
     };
