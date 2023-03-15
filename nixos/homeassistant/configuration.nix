5f942355023bd6582270810e8bdead0453867c01
nokogiri <nokogiri@gefjon.org>
2023-03-15 01:03:55 +0100

    vaapi for jellyfin

--- nixos/homeassistant/configuration.nix
+++ nixos/homeassistant/configuration.nix
@@ -59,8 +59,15 @@
     podman-compose
   ];
 
-  hardware = { };
-
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      #intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      #vaapiVdpau
+      #libvdpau-va-gl
+    ];
+  };
   powerManagement = {
     cpuFreqGovernor = "schedutil";
     powertop.enable = false;
