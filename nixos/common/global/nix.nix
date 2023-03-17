69bad581ffb4877611db2782116c913fa52517fd
nokogiri <nokogiri@gefjon.org>
2023-03-17 20:43:59 +0100

    change gc defaults 🞧 swayfx

--- nixos/common/global/nix.nix
+++ nixos/common/global/nix.nix
@@ -4,16 +4,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
-        #"https://hyprland.cachix.org"
         "https://nookogiri.cachix.org"
-        #"https://nixpkgs-wayland.cachix.org"
         "https://cache.fishoeder.net"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nookogiri.cachix.org-1:tKY3HBn6Xhuqys5EcQRl1u+sKualOXFzCs9Q6qEXR+o="
-        #"nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "cache.fishoeder.net:8LJqtsUc2Sdbvucf7HCakDEetd1b2rGLFmVaDF1POdc="
       ];
       trusted-users = [ "root" "@wheel" ];
@@ -23,7 +19,7 @@
     };
     package = pkgs.nixUnstable;
     gc = {
-      automatic = true;
+      automatic = lib.mkDefault true;
       dates = "weekly";
     };
 
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -19,5 +19,6 @@
   RBDOOM-3-BFG-vk = pkgs.callPackage ./RBDOOM-3-BFG-vk { };
   ryujinx = pkgs.callPackage ./ryujinx { };
   ryzen-ppd = pkgs.callPackage ./ryzen-ppd { };
+  sway-unwrapped = pkgs.callPackage ./swayfx {};
   waydroid = pkgs.callPackage ./waydroid {};
 }
