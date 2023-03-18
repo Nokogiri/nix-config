21f76c70aec77ee2ce3da418e7ac289b319e3fea
nokogiri <nokogiri@gefjon.org>
2023-03-18 21:39:19 +0100

    nix flake update

--- nixos/common/optional/systemd-network.nix
+++ nixos/common/optional/systemd-network.nix
@@ -3,7 +3,7 @@
     enable = true;
     wait-online = {
       timeout = 20;
-      anyInterface = true;
+      anyInterface = false;
       ignoredInterfaces = [ "haos" "veth0" "podman0" "veth0@if2" ];
     };
   };
--- nixos/homeassistant/services/systemd-network.nix
+++ nixos/homeassistant/services/systemd-network.nix
@@ -1,7 +1,7 @@
 {
   systemd.network.networks."20-wired" = {
     matchConfig.Name = [ "en*" ];
-    DHCP = "no";
+    DHCP = "yes";
     address = [ "192.168.178.57" ];
     gateway = [ "192.168.178.1" ];
     dns = [ "192.168.178.254" ];
