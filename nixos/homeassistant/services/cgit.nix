e65ccaa0045f4a6e113735ceeb97379ae921bdf8
nokogiri <nokogiri@gefjon.org>
2023-03-22 23:31:58 +0100

    gitolite group

--- nixos/homeassistant/services/cgit.nix
+++ nixos/homeassistant/services/cgit.nix
@@ -4,7 +4,17 @@
     enable = true;
     nginx.location = "/";
     scanPath = "/var/lib/gitolite";
+    #repos = {
+      #nix-config = {
+        #path = "/var/lib/gitolite/repositories/nix-config.git";
+    #    path = "/var/lib/gitolite/repositories/nix-config.git";
+    #    desc = "nixos configuration";
+        #owner = "nokogiri";
+    #  };
+    #};
     settings = {
+      root-desc = "Git?";
+      remove-suffix = true;
       css = "https://files.fishoeder.net/css/custom.css";
       enable-follow-links = true;
       source-filter = "${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py";
