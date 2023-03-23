ceac2accccee8efc70904f75409434f490a79176
nokogiri <nokogiri@gefjon.org>
2023-03-23 11:14:57 +0100

    cgit

--- nixos/common/optional/gitolite.nix
+++ nixos/common/optional/gitolite.nix
@@ -3,6 +3,7 @@
     enable = true;
     adminPubkey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExoigGlfblca2iJPTcyRc/bL5hqHuDlXmbBf/9PhVKZ nokogiri@frankenbook";
+    description = "nokogiri";
     user = "git";
     group = "http";
     extraGitoliteRc = ''
--- nixos/homeassistant/services/cgit.nix
+++ nixos/homeassistant/services/cgit.nix
@@ -3,20 +3,27 @@
     package = pkgs.cgit-pink;
     enable = true;
     nginx.location = "/";
-    scanPath = "/var/lib/gitolite";
-    #repos = {
-      #nix-config = {
-        #path = "/var/lib/gitolite/repositories/nix-config.git";
-    #    path = "/var/lib/gitolite/repositories/nix-config.git";
-    #    desc = "nixos configuration";
-        #owner = "nokogiri";
-    #  };
-    #};
+    #scanPath = "/var/lib/gitolite";
+    repos = {
+      nix-config = {
+        path = "/var/lib/gitolite/repositories/nix-config.git";
+        desc = "nixos configuration";
+        owner = "nokogiri";
+      };
+      nixpkgs = {
+        path = "/var/lib/gitolite/repositories/nixpkgs.git";
+        desc = "fork of https://github.com/NixOS/nixpkgs";
+        owner = "nokogiri";
+      };
+    };
     settings = {
       root-desc = "Git?";
       remove-suffix = true;
+      clone-url = "https://github.com/Nokogiri/$CGIT_REPO_URL git@git.fishoeder.net:$CGIT_REPO_URL";
       css = "https://files.fishoeder.net/css/custom.css";
       enable-follow-links = true;
+      enable-git-config = true;
+      enable-git-clone = true;
       source-filter = "${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py";
     };
   };
