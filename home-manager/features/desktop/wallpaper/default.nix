5ab96527c387dd4a3532bc97bf086c9c0fb40120
nokogiri <nokogiri@gefjon.org>
2023-03-26 09:06:47 +0200

    wpaperd

--- home-manager/features/desktop/wallpaper/default.nix
+++ home-manager/features/desktop/wallpaper/default.nix
@@ -10,7 +10,7 @@
     apply-shadow = true
 
     [eDP-1]
-    path = "/home/nokogiri/.local/share/wallpaper/Street.webp"
+    path = "/home/nokogiri/.local/share/wallpaper/greencastle.webp"
     apply-shadow = true
   '';
 }
