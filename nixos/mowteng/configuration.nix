75bc58585e8bb01524ef03e05d7850e8ab8bf935
nokogiri <nokogiri@gefjon.org>
2023-03-27 17:24:42 +0200

    libvirt

--- nixos/mowteng/configuration.nix
+++ nixos/mowteng/configuration.nix
@@ -14,10 +14,10 @@
     ../common/optional/avahi.nix
     ../common/optional/bluetooth.nix
     ../common/optional/desktop-wlr.nix
-    #../common/optional/distrobox.nix
     ../common/optional/fonts.nix
     ../common/optional/fprintd.nix
     ../common/optional/greetd.nix
+    ../common/optional/libvirt.nix
     ../common/optional/networkmanager.nix
     ../common/optional/pipewire.nix
     ../common/optional/quietboot.nix
@@ -25,7 +25,6 @@
     ../common/optional/ryzen-ppd.nix
     ../common/optional/sane.nix
     ../common/optional/systemd-boot.nix
-    #../common/optional/waydroid.nix
     ../common/optional/xdg-portal.nix
 
     ../common/users/nokogiri.nix
