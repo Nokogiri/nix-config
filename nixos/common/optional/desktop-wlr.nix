6c3d21b3203dcfee3654a28a33f910dbdae085f9
nokogiri <nokogiri@gefjon.org>
2023-04-03 18:04:16 +0200

    hyprland--

--- nixos/common/optional/desktop-wlr.nix
+++ nixos/common/optional/desktop-wlr.nix
@@ -41,12 +41,12 @@ in {
   qt.style = "kvantum-dark";
   programs = {
     dconf.enable = true;
-    hyprland = {
-      enable = true;
-      xwayland = {
-        enable = true;
-        hidpi = false;
-      };
+  #  hyprland = {
+  #    enable = true;
+  #    xwayland = {
+  #      enable = true;
+  #      hidpi = false;
+  #    };
     };
   };
 
