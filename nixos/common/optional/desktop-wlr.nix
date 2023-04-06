6ef3281d7c0ff8046ca86caa279ebf9002763a60
nokogiri <nokogiri@gefjon.org>
2023-04-06 19:55:36 +0200

    cleaning

--- nixos/common/optional/desktop-wlr.nix
+++ nixos/common/optional/desktop-wlr.nix
@@ -42,13 +42,13 @@ in {
   qt.style = "kvantum-dark";
   programs = {
     dconf.enable = true;
-    hyprland = {
-      enable = true;
-      xwayland = {
-        enable = true;
-        hidpi = false;
-      };
-    };
+    #hyprland = {
+    #  enable = true;
+    #  xwayland = {
+    #    enable = true;
+    #    hidpi = false;
+    #  };
+    #};
   };
 
   services = {
