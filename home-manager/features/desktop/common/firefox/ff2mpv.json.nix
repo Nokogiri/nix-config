30317c0a309ec192401d7d8908d2c58718bff711
nokogiri <nokogiri@gefjon.org>
2023-04-18 11:22:08 +0200

    nix flake update

--- home-manager/features/desktop/common/firefox/ff2mpv.json.nix
+++ home-manager/features/desktop/common/firefox/ff2mpv.json.nix
@@ -1,9 +1,9 @@
-{
+{config, ...}:{
   home.file.".mozilla/native-messaging-hosts/ff2mpv.json".text = ''
     {
       "name": "ff2mpv",
       "description": "ff2mpv's external manifest",
-      "path": "/etc/profiles/per-user/nokogiri/bin/ff2mpv.py",
+      "path": "/home/${config.home.username}/.nix-profile/bin/ff2mpv.py",
       "type": "stdio",
       "allowed_extensions": ["ff2mpv@yossarian.net"]
     }
