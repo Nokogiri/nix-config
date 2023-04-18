06d3246f6ff4f4322e034103b936772347abbe43
nokogiri <nokogiri@gefjon.org>
2023-04-18 20:12:37 +0200

    nix flake update

--- nixos/homeassistant/services/snapserver.nix
+++ nixos/homeassistant/services/snapserver.nix
@@ -4,8 +4,18 @@
     enable = true;
     tcp.enable = true;
     openFirewall = true;
+    http = {
+      listenAddress = "0.0.0.0";
+      enable = true;
+      docRoot = "${pkgs.snapcast}/share/snapserver/snapweb";
+    };
     streams = {
-      "MichaSpotify" = {
+      "AllSpotify" = {
+        type = "librespot";
+        location = "${pkgs.librespot}/bin/librespot";
+        codec = "flac";
+      };
+      "KucheSPotify" = {
         type = "librespot";
         location = "${pkgs.librespot}/bin/librespot";
         codec = "flac";
