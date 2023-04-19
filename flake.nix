ed000739a32f1a51ab072e43f19ec4cb7833c75d
nokogiri <nokogiri@gefjon.org>
2023-04-19 10:03:38 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    masterpkgs.url = "github:nixos/nixpkgs/master";
+    #masterpkgs.url = "github:nixos/nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
