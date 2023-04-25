ce839655edeb8b6a16941066e926d454b5387376
nokogiri <nokogiri@gefjon.org>
2023-04-25 14:02:46 +0200

    mocha

--- home-manager/features/cli/bat/default.nix
+++ home-manager/features/cli/bat/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   xdg.configFile."bat/themes/Catppuccin-mocha.tmTheme".source =
-    ./bat-Catppuccin-frappe.tmTheme;
+    ./bat-Catppuccin-mocha.tmTheme;
   programs.bat = {
     enable = true;
     config.theme = "Catppuccin-mocha";
