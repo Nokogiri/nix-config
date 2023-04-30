415bb974942c9bfab0ef6663bbe371038c94a28f
nokogiri <nokogiri@gefjon.org>
2023-04-30 10:16:01 +0200

    wireguard rework

--- nixos/common/optional/wireguard-server.nix
+++ nixos/common/optional/wireguard-server.nix
@@ -3,13 +3,12 @@
 {
   #sops.defaultSopsFile = ../../common/secrets.yaml;
   sops.secrets = {
-    "wg_private/calvin" = { };
-    "wg_psk/consolero" = { };
-    #"wg_psk/homeassistant" = { };
-    "wg_psk/kathi" = { };
-    "wg_psk/mowteng" = { };
-    "wg_psk/mrpig" = { };
-    "wg_psk/sm-af530" = { };
+    "wg/homeassistant" = { };
+    "psk_wg/showxating" = { };
+    "psk_wg/kipad" = { };
+    "psk_wg/kiphone" = { };
+    "psk_wg/mowteng" = { };
+    "psk_wg/dewalt" = { };
   };
 
   networking.nat.enable = true;
@@ -21,44 +20,38 @@
     haos = {
       ips = [ "10.200.200.1/24" ];
       listenPort = 51871;
-      privateKeyFile = config.sops.secrets."wg_private/calvin".path;
+      privateKeyFile = config.sops.secrets."wg/homeassistant".path;
 
       peers = [
         {
-          # consolero
-          allowedIPs = [ "10.200.200.5/32" "10.200.200.0/24" ];
-          publicKey = "ayew5N9nQFBl25vJHg5DOwOzVQAmJ2DS6wghNAwqtCg=";
-          presharedKeyFile = config.sops.secrets."wg_psk/consolero".path;
-        }
-        #{
-        #  # homeassistant
-        #  allowedIPs = [ "10.200.200.2/32" "10.200.200.0/24" ];
-        #  publicKey = "40RkpFdcTlk+AWc9XC/yhWsnwDJKIhEY7t5z3meFJkQ=";
-        #  presharedKeyFile = config.sops.secrets."wg_psk/homeassistant".path;
-        #}
-        {
           # mowteng
-          allowedIPs = [ "10.200.200.3/32" "10.200.200.0/24" ];
-          publicKey = "PIofiSw2cfcn23NlGC6uJocthPIJqwXB6dUnh4fWwhY=";
-          presharedKeyFile = config.sops.secrets."wg_psk/mowteng".path;
+          allowedIPs = [ "10.200.200.30/32" "10.200.200.0/24" ];
+          publicKey = "CbRmUgGLZuw0Hj7uYFK4aetwePTSzMQEwNsLibFQOX4=";
+          presharedKeyFile = config.sops.secrets."psk_wg/mowteng".path;
         }
         {
-          # kathi
-          allowedIPs = [ "10.200.200.4/32" "10.200.200.0/24" ];
-          publicKey = "KH6v0VRW9o4bzfnE1IRI6/e7lAlU52SkKCp22SXon2Y=";
-          presharedKeyFile = config.sops.secrets."wg_psk/kathi".path;
+          # showxating
+          allowedIPs = [ "10.200.200.31/32" "10.200.200.0/24" ];
+          publicKey = "/akEJ+EEVS5MBB4apmu7XT+seuT8YsuG31reUujqsRg=";
+          presharedKeyFile = config.sops.secrets."psk_wg/showxating".path;
         }
         {
-          # mrpig
-          allowedIPs = [ "10.200.200.7/32" "10.200.200.0/24" ];
-          publicKey = "IqH0Yscrwv3QfQIiuK2v2QGHJDQEtYvSbLYX2alh3Fs=";
-          presharedKeyFile = config.sops.secrets."wg_psk/mrpig".path;
+          # kipad
+          allowedIPs = [ "10.200.200.32/32" "10.200.200.0/24" ];
+          publicKey = "UXb5RaWG2lVlN13mszJMcSHhP6YZdqG+mnHRCE9zcXg=";
+          presharedKeyFile = config.sops.secrets."psk_wg/kipad".path;
+        }
+        { 
+          # kiphone
+          allowedIPs = [ "10.200.200.33/32" "10.200.200.0/24" ];
+          publicKey = "pmkcv8JpotbPo+XsIMB3HDxBVqET4YsRJJVAdT2NR3w=";
+          presharedKeyFile = config.sops.secrets."psk_wg/kiphone".path;
         }
         {
-          # sm-af530
-          allowedIPs = [ "10.200.200.10/32" "10.200.200.0/24" ];
-          publicKey = "olauwI++R95O0gucWSPDO9uH2/ufv2AvzAsVsUQ9Jl4=";
-          presharedKeyFile = config.sops.secrets."wg_psk/sm-af530".path;
+          # dewalt/sm-af530
+          allowedIPs = [ "10.200.200.34/32" "10.200.200.0/24" ];
+          publicKey = "6EM5RLfTCZGrFl1tW1oMiaLetrmblj6oqOXQa4J1ZF8=";
+          presharedKeyFile = config.sops.secrets."psk_wg/dewalt".path;
         }
       ];
     };
