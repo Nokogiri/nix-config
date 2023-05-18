ebfc331b8be7cec3b497db5c040e1744c8926b42
nokogiri <nokogiri@gefjon.org>
2023-05-18 11:50:12 +0200

    nix flake update

--- pkgs/cemu/default.nix
+++ pkgs/cemu/default.nix
@@ -33,14 +33,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cemu";
-  version = "2.0-38";
+  version = "2.0-39";
 
   src = fetchFromGitHub {
     owner = "cemu-project";
     repo = "Cemu";
     rev = "v${version}";
-    #hash = "sha256-+2V78G4SDFb6ZQDDorvT13yqnZw2JAObF+WGYMMGYHE=";
-    hash = "sha256-/zTKwgOTLptjUFGmICdNfq8CMEkvKb4NGi3dSKFOHBE=";
+    hash = "sha256-+2V78G4SDFb6ZQDDorvT13yqnZw2JAObF+WGYMMGYHE=";
   };
 
   patches = [
