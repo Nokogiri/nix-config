5f14ec7511f8bca8c7ed2eb4b09cae21714f258b
nokogiri <nokogiri@gefjon.org>
2023-06-02 10:57:04 +0200

    nix flake update

--- nixos/homeassistant/local/dave.nix
+++ nixos/homeassistant/local/dave.nix
@@ -1,12 +1,12 @@
-{ config, lib, pkgs }: {
+{ config, lib, pkgs, ... }: {
   environment = {
     systemPackages = [ pkgs.dave ];
-    etc."dave.yaml" = {
+    etc."dave/config.yaml" = {
       text = ''
         address: "127.0.0.1"
         port: "8765"
         dir: "/srv/webdav"
-        prefix: "dav"
+        #prefix: "/dav"
         users:
           nokogiri:
             password: "$2a$12$RJvmqt/UxojdL/R6gsrWNugwFwJHS6e11KKzXq6TbHg4oWUq1DnXC"
@@ -25,7 +25,7 @@
     serviceConfig = {
       Type = "simple";
       User = "nokogiri";
-      ExecStart = "${pkgs.dave}/bin/dave --config /etc/dave.yaml";
+      ExecStart = "${pkgs.dave}/bin/dave --config /etc/dave/config.yaml";
       Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
     };
   };
