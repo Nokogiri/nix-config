dfc47e789b92ad6635eb4d33a3f13a082579ec09
nokogiri <nokogiri@gefjon.org>
2023-06-08 18:53:38 +0200

    separate boot and efi

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     #masterpkgs.url = "github:nixos/nixpkgs/master";
 
