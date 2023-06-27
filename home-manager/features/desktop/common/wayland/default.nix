23f6e03a780bfc5833162717e17bcdd276b19281
nokogiri <nokogiri@gefjon.org>
2023-06-27 23:54:10 +0200

    misc

--- home-manager/features/desktop/common/wayland/default.nix
+++ home-manager/features/desktop/common/wayland/default.nix
@@ -31,6 +31,8 @@
     QT_PLUGIN_PATH= "$QT_PLUGIN_PATH:/home/nokogiri/.nix-profile/lib/qt-6/plugins";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    XCURSOR_SIZE = "32";
+    XCURSOR_THEME = "Nordzy-cursors";
   };
 
   systemd.user.sessionVariables = {
@@ -41,5 +43,7 @@
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_QPA_PLATFORMTHEME =
       "${config.home.sessionVariables.QT_QPA_PLATFORMTHEME}";
+    XCURSOR_SIZE = "${config.home.sessionVariables.XCURSOR_SIZE}";
+    XCURSOR_THEME = "${config.home.sessionVariables.XCURSOR_THEME}";
   };
 }
