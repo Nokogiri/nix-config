3e7f8298d5f5ac7f6253a1c585b0b251b5fa426d
nokogiri <nokogiri@gefjon.org>
2023-06-29 16:04:13 +0200

    back to nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    #nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
