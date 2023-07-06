59948bdd42e46e8beb38031ba59496ae77eddb56
nokogiri <nokogiri@gefjon.org>
2023-07-07 00:13:49 +0200

    misc

--- home-manager/features/desktop/wallpaper/default.nix
+++ home-manager/features/desktop/wallpaper/default.nix
@@ -8,7 +8,7 @@
     apply-shadow = false
 
     [eDP-1]
-    path = "/home/nokogiri/.local/share/wallpaper/Totoro-catppuccin_frappe.png"
+    path = "/home/nokogiri/.local/share/wallpaper/Totoro-catppuccin_frappe.webp"
     apply-shadow = false
   '';
 }
